ea4a2d382a2835dd45126e61b3e97907c986f532
Monika Viswanathan <vmonika@athenahealth.com>
2018-05-05 23:14:06 +0530

    odd even

---
+++ beginner/OECheck.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class OECheck {
+    public static void main(String args[])
+    {
+           int n;
+            Scanner in = new Scanner(System.in);
+            n = in.nextInt();
+            if(n%2 == 0)
+            {
+                System.out.print("Even");
+            }
+            else
+            {
+                System.out.print("Odd");
+            }
+            
+    }
+}
